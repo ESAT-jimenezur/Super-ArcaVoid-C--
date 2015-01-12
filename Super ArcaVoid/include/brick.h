9d38b1de4f2dd0535d4285973b7c8471051128ce
Jose Luis Jimenez <joseluiszero@gmail.com>
2015-01-12 18:56:46 +0100

    Added Brick Class (Only base code)

---
+++ Super ArcaVoid/include/brick.h
@@ -0,0 +1,22 @@
+/* Default Brick class declaration
+*
+* @author Jose Luis Jimenez Urbano | < ijos@ijos.es > | < @iJos >
+*/
+
+#ifndef __BRICK_H__
+#define __BRICK_H__
+
+#include "object.h"
+
+class Brick : public Object{
+public:
+  Brick();
+  ~Brick(){};
+
+
+
+private:
+
+};
+
+#endif __BRICK_H__
